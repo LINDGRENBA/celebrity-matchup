85a0016bab7f2f36065820647a46a0aed8aea695
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-04-30 12:32:40 -0700

    Add initial files and basic html forms.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Must be 18 or older to use celebrity matchin app, if younger, get different page/message
+
+//enter age in prompt
+//if 18+ go to form
+//on submit form, show results, with img of celebrity
+
+// if less than 18
+//show sorry message, hide form and do not show results
+
